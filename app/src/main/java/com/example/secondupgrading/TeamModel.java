eb61a71a4b65e1739f517c195529697edc211716
SteffanoMN <ripplepro999@gmail.com>
2021-05-17 11:26:39 +0700

    First commit files

---
+++ app/src/main/java/com/example/secondupgrading/TeamModel.java
@@ -0,0 +1,11 @@
+package com.example.secondupgrading;
+
+public class TeamModel {
+    private String TeamImage, TeamName, TeamDesc;
+
+    public TeamModel(String teamImage, String teamName, String teamDesc) {
+        this.TeamImage = teamImage;
+        this.TeamName = teamName;
+        this.TeamDesc = teamDesc;
+    }
+}
