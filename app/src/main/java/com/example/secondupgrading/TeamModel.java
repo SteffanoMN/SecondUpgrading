c8752d95297daf71812f57cec9ed6823f7ed01d1
SteffanoMN <ripplepro999@gmail.com>
2021-05-18 20:35:59 +0700

    modified files

--- app/src/main/java/com/example/secondupgrading/TeamModel.java
+++ app/src/main/java/com/example/secondupgrading/TeamModel.java
@@ -8,4 +8,28 @@ public class TeamModel {
         this.TeamName = teamName;
         this.TeamDesc = teamDesc;
     }
+
+    public String getTeamImage() {
+        return TeamImage;
+    }
+
+    public void setTeamImage(String teamImage) {
+        TeamImage = teamImage;
+    }
+
+    public String getTeamName() {
+        return TeamName;
+    }
+
+    public void setTeamName(String teamName) {
+        TeamName = teamName;
+    }
+
+    public String getTeamDesc() {
+        return TeamDesc;
+    }
+
+    public void setTeamDesc(String teamDesc) {
+        TeamDesc = teamDesc;
+    }
 }
